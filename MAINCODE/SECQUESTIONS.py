7043b0afb96813105483c1e33a025f0a1f80a304
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-10 13:01:52 +0000

    first commit

---
+++ MAINCODE/SECQUESTIONS.py
@@ -0,0 +1,3 @@
+SECQUESTION1=input("Enter all answers to these questions: Whats your dogs name? Whats your mothers name? What city did you live in?")
+if SECQUESTION1 == "Bob Mary Birmingham":
+    print("Access granted")
